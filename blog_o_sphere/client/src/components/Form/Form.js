9351218499fb09a73f91ab4bc323e8aae00e2d56
Jyotirmay Pathak <jyotirmay362@gmail.com>
2021-03-29 09:44:08 +0530

    Project completed

---
+++ blog_o_sphere/client/src/components/Form/Form.js
@@ -0,0 +1,80 @@
+import React, {useState, useEffect } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import FileBase from 'react-file-base64';
+import { useDispatch, useSelector } from 'react-redux';
+import { createPost, updatePost } from '../../actions/posts';
+import useStyles from './styles';
+
+const Form = ({ currentId, setCurrentId }) => {
+    const [postData, setPostData] = useState({
+        
+        title: '', message: '', tags: '', selectFile: ''
+    })
+    const post = useSelector((state) => currentId ? state.posts.find((message) => message._id === currentId) : null );
+    const classes = useStyles();
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if (currentId === 0) {
+            dispatch(createPost({ ...postData, name: user?.result?.name }))
+            clear();
+        }else{
+            dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }))
+            clear();
+        } 
+    }
+
+    useEffect(() => {
+        if(post) setPostData(post);
+    }, [post])
+
+    const dispatch = useDispatch();
+
+    const clear =() => {
+        setCurrentId(0);
+        setPostData({title:'', message:'', tags:'', selectFile:''})
+    }
+
+    if(!user?.result?.name){
+        return(
+            <Paper className={classes.paper}>
+                <Typography variant="h6" align="center">
+                    Please Sign In to Create your blog.
+                </Typography>
+            </Paper>
+        )
+    }
+
+    return (
+       <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography variant="h6">
+                    {currentId ? 'Editing' : 'Creating'} a Blog
+                </Typography>
+                
+                {/* <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}></TextField> */}
+                
+                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}></TextField>
+                
+                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}></TextField>
+                
+                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })}></TextField>
+                
+                <div className={classes.fileInput}>
+                    <FileBase
+                        type="file"
+                        multiple={false}
+                        onDone={({base64}) => setPostData({...postData, setlectedFile: base64})}>
+                    </FileBase>
+                    
+                </div>
+                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button> 
+                <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button> 
+            </form>
+       </Paper>
+    )
+}
+
+export default Form
---
+++ blog_o_sphere/client/src/reducers/posts.js
@@ -0,0 +1,16 @@
+// import Posts from "../components/Posts/Posts"
+export default (posts= [], action) => {
+     switch (action.type) {
+        case 'DELETE' :
+        case 'LIKE' :
+            return posts.filter((post) => post._id !== action.payload);
+        case 'FETCH_ALL':
+            return action.payload;
+        case 'CREATE':
+            return [...posts, action.payload];
+        case 'UPDATE':
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+        default:
+            return posts;
+     }
+}
